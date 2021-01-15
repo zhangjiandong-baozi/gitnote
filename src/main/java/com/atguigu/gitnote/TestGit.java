5d352929da9fe8cfe14b6b8ee5b18fc149a89e7f
abao <abao@qq.com>
2021-01-15 18:54:19 +0800

    首次提交

---
+++ src/main/java/com/atguigu/gitnote/TestGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.gitnote;
+
+/**
+ * @author mangguodong
+ * @create 2021-01-15
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

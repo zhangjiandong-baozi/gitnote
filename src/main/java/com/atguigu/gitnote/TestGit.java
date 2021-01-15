12fc0c0253d16ca00525b43c9aa77b0e50f1fcb7
jeff abao <52601730+zhangjiandong-baozi@users.noreply.github.com>
2021-01-15 19:09:03 +0800

    Update TestGit.java

--- src/main/java/com/atguigu/gitnote/TestGit.java
+++ src/main/java/com/atguigu/gitnote/TestGit.java
@@ -8,5 +8,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello second");
+        System.out.println("test pull");
+
     }
 }

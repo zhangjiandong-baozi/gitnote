43adb142cf45c2ca62bd75cc1c8bbc7e89a56459
abao <abao@qq.com>
2021-01-15 19:03:05 +0800

    第二次提交

--- src/main/java/com/atguigu/gitnote/TestGit.java
+++ src/main/java/com/atguigu/gitnote/TestGit.java
@@ -7,5 +7,6 @@ package com.atguigu.gitnote;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello second");
     }
 }

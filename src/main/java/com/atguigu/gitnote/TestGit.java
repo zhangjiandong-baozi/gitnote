010b8cd521b248f83f287df941c3723145e32470
jeff abao <52601730+zhangjiandong-baozi@users.noreply.github.com>
2021-01-15 20:39:15 +0800

    Update TestGit.java

--- src/main/java/com/atguigu/gitnote/TestGit.java
+++ src/main/java/com/atguigu/gitnote/TestGit.java
@@ -13,6 +13,7 @@ public class TestGit {
         System.out.println("test push hot-fix2");
         System.out.println("test merge hot-fix");
         System.out.println("test fetch");
+        System.out.println("test fetch2");
 
     }
 }
